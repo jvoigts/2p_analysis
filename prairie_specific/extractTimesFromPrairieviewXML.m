253ef8db150e3140e6645c9907a085cc861e7603
jvoigts <jvoigts@mit.edu>
2015-03-31 09:33:12 -0400

    initial commit likely crashes at end of file - add check for that

---
+++ prairie_specific/extractTimesFromPrairieviewXML.m
@@ -0,0 +1,36 @@
+[relativeTime,absoluteTime]=extractTimesFromPrairieviewXML(directory);
+%
+% [relativeTime,absoluteTime]=extractTimesFromPrairieviewXML(directory);
+%
+% automatically locates xml name in directory - change this if more than
+% one xml is in there
+%
+
+d=dir([directory,'/*.xml'])
+
+% xmlread(fullfile(basedir,d(1).name)); % blows up
+
+% instead, parse for instances of <Frame relativeTime="0"
+
+try % just in case
+    fclose(f);
+end
+
+f=fopen(fullfile(basedir,d(1).name),'r');
+tline= ' ';
+c=0;
+absoluteTime=[];
+relativeTime=[];
+while numel(tline)>0
+    tline = fgets(f);
+    s=  sscanf(strtrim(tline), ['<Frame relativeTime="%f" absoluteTime="%f']);
+    
+    if numel(s)>0
+        c=c+1;
+        relativeTime(c)=s(1);
+        absoluteTime(c)=s(2);
+    end;
+    if mod(c,1000)==0;
+        fprintf('got %d timestamps\n',c);
+    end;
+end;
